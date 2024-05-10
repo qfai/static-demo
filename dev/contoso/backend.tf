7bd209315ac9ba333031092066dec9bb15fd5f4f
qfai <quke@microsoft.com>
2024-05-10 16:44:49 +0800

    export

--- dev/contoso/backend.tf
+++ dev/contoso/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "iacdemo"
     storage_account_name = "contosobackend"
     container_name       = "exportdemo"
-    key                  = "firstsite.tfstate"
+    key                  = "contoso.tfstate"
   }
 }
