4e9cbcd819b0f82c27ae6b31ec5bd722995b346a
qfai <quke@microsoft.com>
2024-05-10 16:49:26 +0800

    export

--- dev/iac142/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "iacdemo"
-    storage_account_name = "contosobackend"
-    container_name       = "exportdemo"
-    key                  = "iac142.tfstate"
-  }
-}
